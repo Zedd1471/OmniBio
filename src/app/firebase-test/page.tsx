bc9bc36a1b2847eb160b8a4c63e587a9e8c6e447
zedd1471 <zayyaddahiru@gmail.com>
2025-05-30 17:56:41 +0100

    Fix: removed or corrected FirebaseAuthDemo import

--- src/app/firebase-test/page.tsx
+++ src/app/firebase-test/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import React from "react";
-import FirebaseAuthDemo from "../../components/FirebaseAuthDemo";
 
 export default function FirebaseTestPage() {
   return (
     <div>
       <h1>Firebase Anonymous Auth Demo</h1>
-      <FirebaseAuthDemo />
     </div>
   );
 }
