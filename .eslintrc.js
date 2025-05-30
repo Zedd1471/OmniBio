45fb48810d1736906d1ac79ce68f733f18124d5a
zedd1471 <zayyaddahiru@gmail.com>
2025-05-30 23:40:06 +0100

    Fix: loosen ESLint rules for Vercel build

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+  extends: 'next',
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-img-element': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
